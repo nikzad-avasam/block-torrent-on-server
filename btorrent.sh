28dd8891e8a8bc744225d6b8623f422aeb72531b
Nikzad <abbas.nikzad70@gmail.com>
2023-04-22 11:50:43 +0330

    Update btorrent.sh

--- btorrent.sh
+++ btorrent.sh
@@ -1,1 +1,26 @@
 
+#!/bin/bash
+#
+# Credit to original author: sam https://github.com/nikzad-avasam/block-torrent-on-server
+# GitHub:   https://github.com/nikzad-avasam/block-torrent-on-server
+# Author:   sam nikzad
+
+echo -n "Blocking all torrent trafic on your server. pls wait ... "
+wget -q -O/etc/trackers https://raw.githubusercontent.com/nikzad-avasam/block-torrent-on-server/main/domains
+cat >/etc/cron.daily/denypublic<<'EOF'
+IFS=$'\n'
+L=$(/usr/bin/sort /etc/trackers | /usr/bin/uniq)
+for fn in $L; do
+        /sbin/iptables -D INPUT -d $fn -j DROP
+        /sbin/iptables -D FORWARD -d $fn -j DROP
+        /sbin/iptables -D OUTPUT -d $fn -j DROP
+        /sbin/iptables -A INPUT -d $fn -j DROP
+        /sbin/iptables -A FORWARD -d $fn -j DROP
+        /sbin/iptables -A OUTPUT -d $fn -j DROP
+done
+EOF
+chmod +x /etc/cron.daily/denypublic
+curl -s -LO https://raw.githubusercontent.com/nikzad-avasam/block-torrent-on-server/main/Thosts
+cat hostsTrackers >> /etc/hosts
+sort -uf /etc/hosts > /etc/hosts.uniq && mv /etc/hosts{.uniq,}
+echo "${OK}"
